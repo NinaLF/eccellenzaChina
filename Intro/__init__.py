ccee78a35cbc4e6941cbbc6b94c6a3a29d662597
Nina Frings <nina.frings@unibas.ch>
2024-10-24 10:44:01 +0200

    new

--- Intro/__init__.py
+++ Intro/__init__.py
@@ -20,8 +20,38 @@ class Player(BasePlayer):
     
     dataScience = models.BooleanField(initial=False)
     dataTeach = models.BooleanField(initial=False)
+
+    age = models.IntegerField(label='How old are you', min=18, max=90)
+    
+    gender = models.StringField( label='How do you identify?',
+        choices=[['Male', 'Male'], ['Female', 'Female'], 
+        ['prefer not to answer/ diverse', 'prefer not to answer/ diverse']],
+        widget = widgets.RadioSelect
+    )
+    education = models.StringField( label='What is your <b>highest education</b>?',
+        choices=[['No formal education', 'No formal education'],
+                ['Compulsory education', 'Compulsory education (secondary school)'], 
+                 ['Further education', 'Further education'],
+                 ['Higher education (Bachelor, Master, PhD)', 'Higher education (Bachelor, Master, PhD)']],
+                 widget = widgets.RadioSelect
+    )
+
+    income = models.StringField(
+                                label='How high is your <b>yearly personal income before tax </b>?',
+        choices=[['< 18.000£', '< 18.000£'],
+                 ['18.000£ to 23.000£', '18.000£ to 23.000£'], 
+                 ['23.001£ to 30.500£', '23.001£ to 30.500£'], 
+                 ['30.501£ to 45.000£', '30.500£ to 45.000£'], 
+                 ['> 45.001£', '> 45.001£']],
+                  widget = widgets.RadioSelect
+    )
     
     
+    subjectiveKnowledge = models.StringField( label= 'How much do you feel you know about how many CO<sub>2</sub> emissions are caused by differen actions?',
+                                              choices=[['1', 'not much at all (1)'], ['2', '3'],['3', '3'],['4', '4'],
+                                                       ['5', '5'], ['6', '6'],  ['7', '7'], ['8', '8'],['9', '9'], ['10', 'A great deal (10)'] ]   )
+
+
 # FUNCTIONS
 # PAGES
 
@@ -40,10 +70,14 @@ class Consent(Page):
 
 class Instructions(Page):
     form_model = 'player'
+
+class Demographics(Page):
+    form_model = 'player'
+    form_fields = ['age', 'gender', 'education', 'income' , 'subjectiveKnowledge']
     
 
 # Page sequence
 page_sequence = [
-    Consent
+    Consent, Demographics
     
 ]
